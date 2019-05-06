90e145eb621feb14e84e51edb42f6e6bf33d4970
王梦 <itlaowang@163.com>
2019-05-06 20:22:56 +0800

    build: 发版1

---
+++ build/precache-manifest.f8e27bef225989286afa4a5c901bd5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbb0a648bdee629b69c8db86120d4b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6ac02f016b60c3c25e4",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "10e7ac640dc5b72e07a9",
+    "url": "/static/css/main.e5d868e2.chunk.css"
+  },
+  {
+    "revision": "f6ac02f016b60c3c25e4",
+    "url": "/static/js/2.ba3491e6.chunk.js"
+  },
+  {
+    "revision": "10e7ac640dc5b72e07a9",
+    "url": "/static/js/main.d22f26f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
